36ccb7eacb2426f91151c194e657297b169a3811
michael-zhao <michael.xin.zhao@gmail.com>
2019-10-15 14:59:41 -0400

    some minor qol & clarity changes

--- time_difference.py
+++ time_difference.py
@@ -1,18 +1,24 @@
-from datetime import datetime, timedelta
+"""
+Module with a method that calculates the difference (in weeks, for now) between two dates.
+"""
+from datetime import datetime
 import sys
 import math
 
 def main():
+    """Main method."""
     in1 = sys.argv[1]
     in2 = sys.argv[2]
     if in1 == "today":
         d1 = datetime.today()
         week_diff = date_difference(d1, datetime.strptime(in2, '%m/%d/%Y'))
     else:
-        week_diff = date_difference(datetime.strptime(in1, '%m/%d/%Y'), datetime.strptime(in2, '%m/%d/%Y'))
+        week_diff = date_difference(
+            datetime.strptime(in1, '%m/%d/%Y'), datetime.strptime(in2, '%m/%d/%Y'))
     print(week_diff)
 
 def date_difference(d1, d2):
+    """Returns the date difference."""
     return math.floor(abs((d2-d1).days / 7))
 
 main()
