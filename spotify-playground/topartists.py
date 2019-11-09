f9538155db68ac9cd06472b3bf0ebd000f2454c6
michael-zhao <michael.xin.zhao@gmail.com>
2019-11-09 16:39:15 -0500

    cleaned up topartists.py, put all the functionality in methods

--- spotify-playground/topartists.py
+++ spotify-playground/topartists.py
@@ -7,109 +7,168 @@ import json
 
 #user id: 1219415681
 
-#used powershell, $env:SPOTIPY_...='...' to make env vars work
+#used powershell, $env:SPOTIPY_CLIENT_ID...='...'
+# $env:SPOTIPY_CLIENT_SECRET='...'
+# $env:SPOTIPY_REDIRECT_URI='...'
 
-#get username from terminal, or else ask for user input username
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    username = input("please enter your username id: ")
+def main():
+    #get username from terminal, or else ask for user input username
+    username = get_username(sys.argv)
+    scope = 'user-top-read'
+    token = util.prompt_for_user_token(username, scope)
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False
+        ranges = ['short_term','medium_term','long_term']
+        track_or_artist = str.lower(input("Do you want to see your top tracks or top artists? "))
+        if track_or_artist == "tracks":
+            get_tracks(token, sp, ranges)
+        elif track_or_artist == "artists":
+            get_artists(token, sp, ranges)
+    else:
+        print("Can't get token for", username)
 
-scope = 'user-top-read'
-token = util.prompt_for_user_token(
-    username, scope, client_id='88c6ac8c789142a3918b399f3b785bc0', 
-    redirect_uri='https://google.com/')
+def get_username(arg):
+    if len(arg) > 1:
+        username = arg[1]
+    else:
+        username = input("please enter your username id: ")
+    return username
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    sp.trace = False
-    ranges = ['short_term','medium_term','long_term']
-    track_or_artist = str.lower(input("Do you want to see your top tracks or top artists? "))
+def get_tracks(token, sp, ranges):
+    term = str.lower(input("What range do you want (short, medium, long)? "))
+    print()
+    leng = int(input("How many songs do you want to show (1-50)? "))
+    print()
 
-    if track_or_artist == "tracks":
-        term = str.lower(input("What range do you want (short, medium, long)? "))
+    if term == "short":
         print()
-        leng = int(input("How many songs do you want to show (1-50)? "))
-        print()
-
-        if term == "short":
-            print()
-            avg = 0
-            print("Range: Short term")
-            results = sp.current_user_top_tracks(time_range=ranges[0],limit=leng)
-            for i, item in enumerate(results['items'],1):
-                avg += item['popularity']
-                print(str(i) + ")", item['name'] + " - " + item['album']['artists'][0]['name'], end="" if len(item['album']['artists']) > 1 else " - popularity: " + str(item['popularity']) + "\n")
-                for x in range(1,len(item['album']['artists'])):
-                    print(", " + item['album']['artists'][x]['name'], end="" if x+1 < len(item['album']['artists']) else " - popularity: " + str(item['popularity']) + "\n")
-            print("average popularity: " + str(avg/leng))
-            print()
-
-        elif term == "medium":
-            print()
-            avg = 0
-            print("Range: Medium term")
-            results = sp.current_user_top_tracks(time_range=ranges[1],limit=leng)
-            for i, item in enumerate(results['items'],1):
-                avg += item['popularity']
-                print(str(i) + ")", item['name'] + " - " + item['album']['artists'][0]['name'], end="" if len(item['album']['artists']) > 1 else " - popularity: " + str(item['popularity']) + "\n")
-                for x in range(1,len(item['album']['artists'])):
-                    print(", " + item['album']['artists'][x]['name'], end="" if x+1 < len(item['album']['artists']) else " - popularity: " + str(item['popularity']) + "\n")
-            print("average popularity: " + str(avg/leng))
-            print()
-
-        elif term == "long":
-            print()
-            avg = 0
-            print("Range: Long term")
-            results = sp.current_user_top_tracks(time_range=ranges[2],limit=leng)
-            for i, item in enumerate(results['items'],1):
-                avg += item['popularity']
-                print(str(i) + ")", item['name'] + " - " + item['album']['artists'][0]['name'], end="" if len(item['album']['artists']) > 1 else " - popularity: " + str(item['popularity']) + "\n")
-                for x in range(1,len(item['album']['artists'])):
-                    print(", " + item['album']['artists'][x]['name'], end="" if x+1 < len(item['album']['artists']) else " - popularity: " + str(item['popularity']) + "\n")
-            print("average popularity: " + str(avg/leng))
-            print()
-
-    if track_or_artist == "artists":
-        term = str.lower(input("What range do you want (short, medium, long)? "))
-        print()
-        artists_range = input("How many artists do you want to show? Enter a number 1-50 (default 10)")
-        leng = int(artists_range) if isinstance(artists_range, int) else 10
+        avg = 0
+        print("Range: Short term")
+        results = sp.current_user_top_tracks(time_range=ranges[0],limit=leng)
+        for i, item in enumerate(results['items'],1):
+            avg += item['popularity']
+            print(str(i) + ")", item['name'] + " - " + item['album']['artists'][0]['name'], end="" if len(item['album']['artists']) > 1 else " - popularity: " + str(item['popularity']) + "\n")
+            for x in range(1,len(item['album']['artists'])):
+                print(", " + item['album']['artists'][x]['name'], end="" if x+1 < len(item['album']['artists']) else " - popularity: " + str(item['popularity']) + "\n")
+        print("average popularity: " + str(avg/leng))
         print()
 
-        if term == "short" or term == "s":
-            print("Range: Short term")
-            results =sp.current_user_top_artists(time_range=ranges[0],limit=leng)
-            for i, item in enumerate(results['items'],1):
-                popu = " - popularity: " + str(item['popularity'])
-                with_genre = item['name'] + popu + "; genres: " + item['genres'][0] if len(item['genres']) > 0 else item['name'] + popu
-                print(str(i) + ")", with_genre, end="" if len(item['genres']) > 1 else "\n")
-                for x in range(1,len(item['genres'])):
-                    print(", " + item['genres'][x], end="" if x+1 < len(item['genres']) else "\n")
-            print()
+    elif term == "medium":
+        print()
+        avg = 0
+        print("Range: Medium term")
+        results = sp.current_user_top_tracks(time_range=ranges[1],limit=leng)
+        for i, item in enumerate(results['items'],1):
+            avg += item['popularity']
+            print(str(i) + ")", item['name'] + " - " + item['album']['artists'][0]['name'], end="" if len(item['album']['artists']) > 1 else " - popularity: " + str(item['popularity']) + "\n")
+            for x in range(1,len(item['album']['artists'])):
+                print(", " + item['album']['artists'][x]['name'], end="" if x+1 < len(item['album']['artists']) else " - popularity: " + str(item['popularity']) + "\n")
+        print("average popularity: " + str(avg/leng))
+        print()
 
-        elif term == "medium" or term == "m" or term == "med":
-            print("Range: Medium term")
-            results = sp.current_user_top_artists(time_range=ranges[1],limit=leng)
-            for i, item in enumerate(results['items'],1):
-                withGenre = item['name'] + "; genres: " + item['genres'][0] if len(item['genres']) > 0 else item['name']
-                print(str(i)+ ")", withGenre, end="" if len(item['genres']) > 1 else "\n")
-                for x in range(1,len(item['genres'])):
-                    print(", " + item['genres'][x], end="" if x+1 < len(item['genres']) else "\n")
-            print()
-
-        elif term == "long" or term == "l":
-            print("Range: Long term")
-            results = sp.current_user_top_artists(time_range=ranges[2],limit=leng)
-            for i, item in enumerate(results['items'],1):
-                withGenre = item['name'] + "; genres: " + item['genres'][0] if len(item['genres']) > 0 else item['name']
-                print(str(i) + ")", withGenre, end="" if len(item['genres']) > 1 else "\n")
-                for x in range(1,len(item['genres'])):
-                    print(", " + item['genres'][x], end="" if x+1 < len(item['genres']) else "\n")
-            print()
-else:
-    print("Can't get token for", username)
+    elif term == "long":
+        print()
+        avg = 0
+        print("Range: Long term")
+        results = sp.current_user_top_tracks(time_range=ranges[2],limit=leng)
+        for i, item in enumerate(results['items'],1):
+            avg += item['popularity']
+            print(str(i) + ")", item['name'] + " - " + item['album']['artists'][0]['name'], end="" if len(item['album']['artists']) > 1 else " - popularity: " + str(item['popularity']) + "\n")
+            for x in range(1,len(item['album']['artists'])):
+                print(", " + item['album']['artists'][x]['name'], end="" if x+1 < len(item['album']['artists']) else " - popularity: " + str(item['popularity']) + "\n")
+        print("average popularity: " + str(avg/leng))
+        print()
 
 
+# if token:
+#     sp = spotipy.Spotify(auth=token)
+#     sp.trace = False
+#     ranges = ['short_term','medium_term','long_term']
+#     track_or_artist = str.lower(input("Do you want to see your top tracks or top artists? "))
 
+#     if track_or_artist == "tracks":
+#         term = str.lower(input("What range do you want (short, medium, long)? "))
+#         print()
+#         leng = int(input("How many songs do you want to show (1-50)? "))
+#         print()
+
+#         if term == "short":
+#             print()
+#             avg = 0
+#             print("Range: Short term")
+#             results = sp.current_user_top_tracks(time_range=ranges[0],limit=leng)
+#             for i, item in enumerate(results['items'],1):
+#                 avg += item['popularity']
+#                 print(str(i) + ")", item['name'] + " - " + item['album']['artists'][0]['name'], end="" if len(item['album']['artists']) > 1 else " - popularity: " + str(item['popularity']) + "\n")
+#                 for x in range(1,len(item['album']['artists'])):
+#                     print(", " + item['album']['artists'][x]['name'], end="" if x+1 < len(item['album']['artists']) else " - popularity: " + str(item['popularity']) + "\n")
+#             print("average popularity: " + str(avg/leng))
+#             print()
+
+#         elif term == "medium":
+#             print()
+#             avg = 0
+#             print("Range: Medium term")
+#             results = sp.current_user_top_tracks(time_range=ranges[1],limit=leng)
+#             for i, item in enumerate(results['items'],1):
+#                 avg += item['popularity']
+#                 print(str(i) + ")", item['name'] + " - " + item['album']['artists'][0]['name'], end="" if len(item['album']['artists']) > 1 else " - popularity: " + str(item['popularity']) + "\n")
+#                 for x in range(1,len(item['album']['artists'])):
+#                     print(", " + item['album']['artists'][x]['name'], end="" if x+1 < len(item['album']['artists']) else " - popularity: " + str(item['popularity']) + "\n")
+#             print("average popularity: " + str(avg/leng))
+#             print()
+
+#         elif term == "long":
+#             print()
+#             avg = 0
+#             print("Range: Long term")
+#             results = sp.current_user_top_tracks(time_range=ranges[2],limit=leng)
+#             for i, item in enumerate(results['items'],1):
+#                 avg += item['popularity']
+#                 print(str(i) + ")", item['name'] + " - " + item['album']['artists'][0]['name'], end="" if len(item['album']['artists']) > 1 else " - popularity: " + str(item['popularity']) + "\n")
+#                 for x in range(1,len(item['album']['artists'])):
+#                     print(", " + item['album']['artists'][x]['name'], end="" if x+1 < len(item['album']['artists']) else " - popularity: " + str(item['popularity']) + "\n")
+#             print("average popularity: " + str(avg/leng))
+#             print()
+
+def get_artists(token, sp, ranges):
+    term = str.lower(input("What range do you want (short, medium, long)? "))
+    print()
+    artists_range = input("How many artists do you want to show? Enter a number 1-50 (default 10)")
+    leng = int(artists_range) if isinstance(artists_range, int) else 10
+    print()
+
+    if term == "short" or term == "s":
+        print("Range: Short term")
+        results =sp.current_user_top_artists(time_range=ranges[0],limit=leng)
+        for i, item in enumerate(results['items'],1):
+            popu = " - popularity: " + str(item['popularity'])
+            with_genre = item['name'] + popu + "; genres: " + item['genres'][0] if len(item['genres']) > 0 else item['name'] + popu
+            print(str(i) + ")", with_genre, end="" if len(item['genres']) > 1 else "\n")
+            for x in range(1,len(item['genres'])):
+                print(", " + item['genres'][x], end="" if x+1 < len(item['genres']) else "\n")
+        print()
+
+    elif term == "medium" or term == "m" or term == "med":
+        print("Range: Medium term")
+        results = sp.current_user_top_artists(time_range=ranges[1],limit=leng)
+        for i, item in enumerate(results['items'],1):
+            withGenre = item['name'] + "; genres: " + item['genres'][0] if len(item['genres']) > 0 else item['name']
+            print(str(i)+ ")", withGenre, end="" if len(item['genres']) > 1 else "\n")
+            for x in range(1,len(item['genres'])):
+                print(", " + item['genres'][x], end="" if x+1 < len(item['genres']) else "\n")
+        print()
+
+    elif term == "long" or term == "l":
+        print("Range: Long term")
+        results = sp.current_user_top_artists(time_range=ranges[2],limit=leng)
+        for i, item in enumerate(results['items'],1):
+            withGenre = item['name'] + "; genres: " + item['genres'][0] if len(item['genres']) > 0 else item['name']
+            print(str(i) + ")", withGenre, end="" if len(item['genres']) > 1 else "\n")
+            for x in range(1,len(item['genres'])):
+                print(", " + item['genres'][x], end="" if x+1 < len(item['genres']) else "\n")
+        print()
+
+
+main()
