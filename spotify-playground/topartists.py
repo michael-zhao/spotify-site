0e595ff70f3fe03b599331c730be7fcb5e0cc9bd
michael-zhao <michael.xin.zhao@gmail.com>
2023-07-23 21:11:07 -0700

    refactored top artists method and added helper

--- spotify-playground/topartists.py
+++ spotify-playground/topartists.py
@@ -2,6 +2,7 @@
 Module for finding the top Spotify artists for a user.
 """
 import sys
+import types
 
 import spotipy  # figured out linting error (module spotipy not found) by changing .vscode/settings.json path
 import spotipy.util as util
@@ -12,9 +13,10 @@ import spotipy.util as util
 # $env:SPOTIPY_CLIENT_SECRET='...'
 # $env:SPOTIPY_REDIRECT_URI='...'
 
-SHORT_TERM = "short"
-MEDIUM_TERM = "medium"
-LONG_TERM = "long"
+consts = types.SimpleNamespace()
+consts.SHORT_TERM = "short"
+consts.MEDIUM_TERM = "medium"
+consts.LONG_TERM = "long"
 
 def main():
     """
@@ -56,7 +58,6 @@ def get_tracks(token, sp, ranges):
     """
     Retrieve the top tracks based on input from user
     """
-
     valid_responses = {"short", "medium", "long"}
     term = str.lower(input("What range do you want (short, medium, long)? "))
     while term not in valid_responses:
@@ -69,25 +70,28 @@ def get_tracks(token, sp, ranges):
         track_range = 10
     leng = track_range
     print()
-    print()
 
     match term:
-        case SHORT_TERM as time_range:
+        case consts.SHORT_TERM as time_range:
             print(time_range)
             results = sp.current_user_top_tracks(time_range=ranges[0], limit=leng)
             print_popularity(results, leng)
-        case MEDIUM_TERM as time_range:
+        case consts.MEDIUM_TERM as time_range:
             print(time_range)
             results = sp.current_user_top_tracks(time_range=ranges[1], limit=leng)
             print_popularity(results, leng)
-        case LONG_TERM as time_range:
+        case consts.LONG_TERM as time_range:
             print(time_range)
             results = sp.current_user_top_tracks(time_range=ranges[2], limit=leng)
             print_popularity(results, leng)
         case _:
             print("Invalid input - how did you get here?")
+    print()
 
 def get_artists(token, sp, ranges):
+    """
+    Retrieve top artists based on input from user.
+    """
     term = str.lower(input("What range do you want (short, medium, long)? "))
     if term != "short" or term != "medium" or term != "long":
         term = str.lower(input("Please enter a valid range: short, medium, or long: "))
@@ -100,50 +104,48 @@ def get_artists(token, sp, ranges):
     leng = artist_range
     print()
 
-    if term == "short" or term == "s":
-        print("Range: Short term")
-        results = sp.current_user_top_artists(time_range=ranges[0], limit=leng)
-        for i, item in enumerate(results['items'], 1):
-            popu = " - popularity: " + str(item['popularity'])
-            with_genre = item['name'] + popu + "; genres: " + item['genres'][0] if len(item['genres']) > 0 else item[
-                                                                                                                    'name'] + popu
-            print(str(i) + ")", with_genre, end="" if len(item['genres']) > 1 else "\n")
-            for x in range(1, len(item['genres'])):
-                print(", " + item['genres'][x], end="" if x + 1 < len(item['genres']) else "\n")
-        print()
-
-    elif term == "medium" or term == "m" or term == "med":
-        print("Range: Medium term")
-        results = sp.current_user_top_artists(time_range=ranges[1], limit=leng)
-        for i, item in enumerate(results['items'], 1):
-            withGenre = item['name'] + "; genres: " + item['genres'][0] if len(item['genres']) > 0 else item['name']
-            print(str(i) + ")", withGenre, end="" if len(item['genres']) > 1 else "\n")
-            for x in range(1, len(item['genres'])):
-                print(", " + item['genres'][x], end="" if x + 1 < len(item['genres']) else "\n")
-        print()
-
-    elif term == "long" or term == "l":
-        print("Range: Long term")
-        results = sp.current_user_top_artists(time_range=ranges[2], limit=leng)
-        for i, item in enumerate(results['items'], 1):
-            withGenre = item['name'] + "; genres: " + item['genres'][0] if len(item['genres']) > 0 else item['name']
-            print(str(i) + ")", withGenre, end="" if len(item['genres']) > 1 else "\n")
-            for x in range(1, len(item['genres'])):
-                print(", " + item['genres'][x], end="" if x + 1 < len(item['genres']) else "\n")
-        print()
+    match term:
+        case consts.SHORT_TERM as time_range:
+            print(time_range)
+            results = sp.current_user_top_artists(time_range=ranges[0], limit=leng)
+            print_artists_helper(results)
+        case consts.MEDIUM_TERM as time_range:
+            print(time_range)
+            results = sp.current_user_top_artists(time_range=ranges[1], limit=leng)
+            print_artists_helper(results)
+        case consts.LONG_TERM as time_range:
+            print(time_range)
+            results = sp.current_user_top_artists(time_range=ranges[2], limit=leng)
+            print_artists_helper(results)
+        case _:
+            print("How did you get here?")
+    print()
 
 def print_popularity(results, leng):
-    sum = 0
+    """
+    Helper method to find and print the popularity of an artist.
+    """
+    sum_for_avg = 0
     for i, item in enumerate(results['items'], 1):
-        sum += item['popularity']
+        sum_for_avg += item['popularity']
         print(str(i) + ")", item['name'] + " - " + item['album']['artists'][0]['name'],
                     end="" if len(item['album']['artists']) > 1 else " - popularity: " + str(item['popularity']) + "\n")
         for x in range(1, len(item['album']['artists'])):
             print(", " + item['album']['artists'][x]['name'],
                     end="" if x + 1 < len(item['album']['artists']) else " - popularity: " + str(
                         item['popularity']) + "\n")
-    print("average popularity of music listened to: " + str(sum / leng))
+    print("average popularity of music listened to: " + str(sum_for_avg / leng))
     print()
+
+def print_artists_helper(results):
+    """
+    Helper method to find and print the top artists of a user.
+    """
+    for i, item in enumerate(results['items'], 1):
+        with_genre = item['name'] + "; genres: " + item['genres'][0] if len(item['genres']) > 0 else item['name']
+        print(str(i) + ")", with_genre, end="" if len(item['genres']) > 1 else "\n")
+        for x in range(1, len(item['genres'])):
+            print(", " + item['genres'][x], end="" if x + 1 < len(item['genres']) else "\n") 
 
 
 main()
